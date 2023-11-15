48d61c999889389a9b78b2c2d53310d4b49e4531
rahul@clearcut.design <rahul@clearcut.design>
2023-11-15 23:46:05 +0530

    ad commit

--- pages/components/Ad.js
+++ pages/components/Ad.js
@@ -3,15 +3,9 @@ import Image from "next/image";
 function Ad({ isLoading, prompt, onButtonClick }) {
   return (
     <div className="flex w-full md:w-3/4 bg-white h-24 flex-col mt-8 justify-center items-center">
-      <div className="flex w-full h-24 justify-center">
-        <Image
-          src="/amazon-banner.png"
-          alt="Sample Ad"
-          layout="responsive"
-          width={300}
-          height={100}
-        />
-      </div>
+      <span className="flex w-full h-24 justify-center text-4xl items-center text-gray-400">
+        ad
+      </span>
     </div>
   );
 }

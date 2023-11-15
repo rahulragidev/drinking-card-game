5ad24a1ec6b86837a6cb77e4e54d2a55e8951999
rahul@clearcut.design <rahul@clearcut.design>
2023-11-15 23:48:21 +0530

    google adsense preview

--- pages/components/Ad.js
+++ pages/components/Ad.js
@@ -4,7 +4,11 @@ function Ad({ isLoading, prompt, onButtonClick }) {
   return (
     <div className="flex w-full md:w-3/4 bg-white h-24 flex-col mt-8 justify-center items-center">
       <span className="flex w-full h-24 justify-center text-4xl items-center text-gray-400">
-        ad
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2566390247263128"
+          crossorigin="anonymous"
+        ></script>
       </span>
     </div>
   );
